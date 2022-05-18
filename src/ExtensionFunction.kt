8171d52988296a3a1c3ddd0a01e8106995b40cc4
Alex <lxkrmr@icloud.com>
2022-05-18 10:49:50 +0200

    added extension function to sum all ints in a list of ints

--- src/ExtensionFunction.kt
+++ src/ExtensionFunction.kt
@@ -15,10 +15,16 @@ infix fun <T> T.eq(other: T) {
     else println("Error: $this != $other")
 }
 
+fun List<Int>.sum() : Int {
+    return this.reduce({ acc, current -> acc + current})
+}
+
 fun main() {
     println("abc".last())
     println("abc".lastCharByMe())
     println("abc".repeat(3))
     42 eq 42
     42 eq 43
+    println(listOf(1, 2, 3).sum())
+    // no sum method on list of strings: listOf("one", "two", "three").sum
 }
