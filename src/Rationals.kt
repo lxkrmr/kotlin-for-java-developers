41e09bd51ee437245d3ea61cc5150ba2ce93d1cc
Alex <lxkrmr@icloud.com>
2022-06-08 10:00:31 +0200

    Added infix divBy function to create Reational

--- src/Rationals.kt
+++ src/Rationals.kt
@@ -63,14 +63,18 @@ operator fun Rational?.div(other: Rational?): Rational {
 
 operator fun Rational.unaryMinus() = Rational.create(-numerator, -denominator)
 
+infix fun Int.divBy(denominator: Int): Rational {
+    return Rational.create(this, denominator)
+}
+
 
 fun main() {
     val oneHalf = Rational.create(1, 2)
-    val oneThird = Rational.create(1, 3)
+    val oneThird = 1 divBy 3
 
-    oneHalf + oneThird eq Rational.create(5, 6) // 1/2 + 1/3 = 3/6 + 2/6 = 5/6
-    oneHalf - oneThird eq Rational.create(1, 6) // 1/2 - 1/3 = 3/6 - 2/6 = 1/6
-    oneThird - oneHalf eq -Rational.create(1, 6) // 1/3 - 1/2 = 2/6 - 3/6 = -(1/6)
-    oneHalf * oneThird eq Rational.create(1, 6) // 1/2 * 1/3 = 1/6
-    oneHalf / oneThird eq Rational.create(3, 2) // 1/2 / 1/3 = 3/2 = 1 1/2
+    oneHalf + oneThird eq (5 divBy 6) // 1/2 + 1/3 = 3/6 + 2/6 = 5/6
+    oneHalf - oneThird eq (1 divBy 6) // 1/2 - 1/3 = 3/6 - 2/6 = 1/6
+    oneThird - oneHalf eq -(1 divBy 6) // 1/3 - 1/2 = 2/6 - 3/6 = -(1/6)
+    oneHalf * oneThird eq (1 divBy 6) // 1/2 * 1/3 = 1/6
+    oneHalf / oneThird eq (3 divBy 2) // 1/2 / 1/3 = 3/2 = 1 1/2
 }
